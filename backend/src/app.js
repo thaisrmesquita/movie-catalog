4bf7d9995167adebfa0217ceebef99a5f6765f5e
Thais RIbeiro <thaisribeiro.mesquita@gmail.com>
2020-11-15 22:17:28 -0300

    Adicionado conexão com banco de dados mongoDB

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
     constructor () {
         this.server = express();
+        mongoose.connect(
+            'mongodb+srv://movie-catalog:movie-catalog@movie-catalog.cxuda.mongodb.net/movie-catalog?retryWrites=true&w=majority',
+            {
+              useNewUrlParser: true,
+              useUnifiedTopology: true,
+            }
+        );
         this.middlewares();
         this.routes();
     }
